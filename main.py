557cce6d48d01a4f60a9741ca6046652d23fb762
EYRAUD Come <ce.eyraud@gmail.com>
2024-01-24 10:46:38 +0100

    Working version

--- main.py
+++ main.py
@@ -7,30 +7,38 @@ from fastapi.staticfiles import StaticFiles
 import subprocess
 import requests
 import re
-
 app = FastAPI()
 central_server = "https://parking-rio.rezel.net/"
 
-def sendplaque(plaque):
-    central_server_url = central_server + "carDetected"  # Replace "endpoint" with the actual endpoint on the central server
-    ans = requests.post(central_server_url, json={"plaque": plaque})
-    print(ans)
+def sendplaque(plaque,direction):
+    central_server_url = central_server + "api/carDetected"  # Replace "endpoint" with the actual endpoint on the central server
+    try:
+        obj = {"plaque": plaque,"direction": direction}
+        ans = requests.post(central_server_url, json=obj)
+        ans.raise_for_status()
+        return ans.json()
+    except requests.exceptions.RequestException as err:
+        return {"error":f"Server error : ${str(err)}"}
+
 
 def most_p_plaque(output):
-    match = re.search(r'\b([A-Z0-9]+)\s+confidence:', output)
-    if match:
-        plate_number = match.group(1)
-        return plate_number
-    else :
+    if output:
+        match = re.search(r'\b([A-Z0-9]+)\s+confidence:', output)
+        if match:
+            plate_number = match.group(1)
+            return plate_number
+        else :
+            return "error"
+    else : 
         return "error"
 
 app.mount("/static", StaticFiles(directory="templates"), name="static")
 
 IMG_NAME = "plaque.png"
-COMMAND = f"alpr -c eu {IMG_NAME}"
+COMMAND = f"bash -c 'source ~/.bashrc && alpr -c eu {IMG_NAME}'"
 
 @app.post("/detection")
-def image_detection(image: Annotated[str, Form()]):
+def image_detection(image: Annotated[str, Form()],direction: Annotated[str, Form()]):
     """do detection"""
     contents = image
     img = base64.b64decode(contents)
@@ -41,8 +49,8 @@ def image_detection(image: Annotated[str, Form()]):
     if plaque == "error":
         return {"message":"No License Plate Found"}
     else:
-        sendplaque(plaque)
-        return {"message": plaque}
+        json = sendplaque(plaque,direction)
+        return {"message": plaque, "direction":direction,"srv":json}
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -53,5 +61,5 @@ async def render_index():
 
 if __name__ == "__main__":
     import uvicorn
-
     uvicorn.run(app, host="127.0.0.1", port=8000)
+    
